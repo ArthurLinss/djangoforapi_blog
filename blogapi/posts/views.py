a15cbcc220cc009f05908cfbe6a55eccf4a9fe48
Linß, Arthur <arthur.linss@dlr.de>
2023-03-06 15:50:14 +0100

    adding self-defined persmissions

--- blogapi/posts/views.py
+++ blogapi/posts/views.py
@@ -1,18 +1,18 @@
 from django.shortcuts import render
-from rest_framework import generics
+from rest_framework import generics, permissions
 from .models import Post
 from .serializers import PostSerializer
 
-
+from .permissions import IsAuthorOrReadOnly # new
 
 class PostList(generics.ListCreateAPIView):
-
+    ##permission_classes = (permissions.IsAuthenticated,) # new
     queryset = Post.objects.all()
     serializer_class = PostSerializer
 
 
 class PostDetail(generics.RetrieveUpdateDestroyAPIView):
-
+    permission_classes = (IsAuthorOrReadOnly,) # new
     queryset = Post.objects.all()
     serializer_class = PostSerializer
 
